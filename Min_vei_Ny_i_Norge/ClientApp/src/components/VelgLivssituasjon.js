d774f8ce9057c2d2a78161b24a217ca6a85dfd49
Marie Spongsveen <marie.spongsveen@gmail.com>
2023-03-27 15:35:49 +0200

    lagt inn ikoner, er vanskelig å putte de inn med lik størrelse, ellers begynt å gjøre den responsiv men fortsatt en del igjen, også lagt på hover funksjon for å gjøre dem oransje

--- Min_vei_Ny_i_Norge/ClientApp/src/components/VelgLivssituasjon.js
+++ Min_vei_Ny_i_Norge/ClientApp/src/components/VelgLivssituasjon.js
@@ -1,47 +1,71 @@
 ﻿import { react } from 'react'
+import barnIkon from '../bilder/barn_ikon.png'
+import buisnessIkon from '../bilder/business_ikon.png'
+import dødArvIkon from '../bilder/død_arv_ikon.png'
+import frivilligIkon from '../bilder/frivillig_ikon.png'
+import jobbIkon from '../bilder/jobb_ikon.png'
+import nyINorgeIkon from '../bilder/ny_norge_ikon.png'
+import syktBarnIkon from '../bilder/sykt_barn_ikon.png'
 import './VelgLivssituasjon.css'
 
 export const VelgLivssituasjon = () => {
     return (
-        <div className="velg-livssituasjon-side">
+        <div>
             <button>Back</button>
 
-            <div className="">
+            <div className="velg-livssituasjon-side">
                 <h1>My Digital Guide</h1>
                 <h2>Choose your life situation</h2>
 
                 <div className="livssituasjonene">
-                    <div className="livssituasjon-kolonne">
-                        <div className="livssituasjon-boks">
+                    <div className="livssituasjon-boks">
+                        <div className="livssituasjon-boks-overskrift">
+                            <img src={nyINorgeIkon}></img>
                             <h3>New in Norway</h3>
-                            <p>You are moving to Norway temporarily or permanently due to work, studies, family or other reasons.</p>
                         </div>
-                        <div className="livssituasjon-boks">
-                            <h3>Have a child</h3>
-                            <p>You are expecting or have recently had a child.</p>
-                        </div>
-                        <div className="livssituasjon-boks">
-                            <h3>Loose and Find a job</h3>
-                            <p>You have lost your job or are looking for new work.</p>
-                        </div>
-                        <div className="livssituasjon-boks">
-                            <h3>Death and inheritance</h3>
-                            <p>You are planning the last phase of someone's life or are a caregiver of someone seriously ill or injured.</p>
-                        </div>
+                        <p>You are moving to Norway temporarily or permanently due to work, studies, family or other reasons.</p>
                     </div>
-                    <div className="livssituasjon-kolonne">
-                        <div className="livssituasjon-boks">
+                    <div className="livssituasjon-boks">
+                        <div className="livssituasjon-boks-overskrift">
+                            <img src={barnIkon} className="barn-ikon"></img>
+                            <h3>Have a child</h3>
+                        </div>
+                        <p>You are expecting or have recently had a child.</p>
+                    </div>
+                    <div className="livssituasjon-boks">
+                        <div className="livssituasjon-boks-overskrift">
+                            <img src={jobbIkon}></img>
+                            <h3>Loose and Find a job</h3>
+                        </div>
+                        <p>You have lost your job or are looking for new work.</p>
+                    </div>
+                    <div className="livssituasjon-boks">
+                        <div className="livssituasjon-boks-overskrift">
+                            <img src={dødArvIkon}></img>
+                            <h3>Death and inheritance</h3>
+                        </div>
+                        <p>You are planning the last phase of someone's life or are a caregiver of someone seriously ill or injured.</p>
+                    </div>
+                    <div className="livssituasjon-boks">
+                        <div className="livssituasjon-boks-overskrift">
+                            <img src={syktBarnIkon}></img>
                             <h3>Seriously ill child</h3>
-                            <p>You have a chil with needs, chronically ill child or child in hospital.</p>
                         </div>
-                        <div className="livssituasjon-boks">
+                        <p>You have a chil with needs, chronically ill child or child in hospital.</p>
+                    </div>
+                    <div className="livssituasjon-boks">
+                        <div className="livssituasjon-boks-overskrift">
+                            <img src={frivilligIkon}></img>
                             <h3>Start and run a voluntary organization</h3>
-                            <p>You want to start your own voluntary organization or you already have one.</p>
                         </div>
-                        <div className="livssituasjon-boks">
+                        <p>You want to start your own voluntary organization or you already have one.</p>
+                    </div>
+                    <div className="livssituasjon-boks">
+                        <div className="livssituasjon-boks-overskrift">
+                            <img src={buisnessIkon}></img>
                             <h3>Start and run a business</h3>
-                            <p>You want to start your own business or you already have your own.</p>
                         </div>
+                        <p>You want to start your own business or you already have your own.</p>
                     </div>
                  </div>
             </div>
