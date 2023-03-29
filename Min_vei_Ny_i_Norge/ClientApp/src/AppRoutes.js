84da25034f00861f9b357da069baa15298ccf033
JonasSolberg <yojonas.solberg@gmail.com>
2023-03-29 15:18:31 +0200

    Lagde en innloggingsiden, men den fungerer ikke
    Compiled with problems:X
    
    ERROR in ./src/AppRoutes.js 16:32-42
    
    export 'Innlogging' (imported as 'Innlogging') was not found in './components/Innlogging' (possible exports: default)

--- Min_vei_Ny_i_Norge/ClientApp/src/AppRoutes.js
+++ Min_vei_Ny_i_Norge/ClientApp/src/AppRoutes.js
@@ -1,10 +1,15 @@
 import { Home } from "./components/Home";
+import { Innlogging } from "./components/Innlogging";
 
 const AppRoutes = [
   {
     index: true,
     element: <Home />
-  },
+    },
+    {
+        path: 'Innlogging',
+        element: < Innlogging />
+    }
 ];
 
 export default AppRoutes;
---
+++ Min_vei_Ny_i_Norge/ClientApp/src/components/Innlogging.js
@@ -0,0 +1,111 @@
+﻿import React, { Component, useState } from 'react';
+import { StyleSheet, View } from 'react';
+
+
+//basert på https://contactmentor.com/login-form-react-js-code/?utm_content=cmp-true
+
+//export class Innlogging extends Component {
+ //   static displayName = Innlogging.name;
+
+function Innlogging() {
+    //react states
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    // user login info
+    const database = [
+        {
+            username: "user1",
+            password: "pass1"
+        },
+        {
+            username: 'user2',
+            password: 'pass2'
+        }
+    ];
+
+    const errors = {
+        uname: 'invald username',
+        pass: 'inavalid password'
+    };
+
+    const handleSubmit = (event) => {
+        //Prevent page reload
+        event.preventDefault();
+
+        var { uname, pass } = document.forms[0];
+
+        // Find user login info
+        const userData = database.find((user) => user.username === uname.value);
+
+        // Compare user info
+        if (userData) {
+            if (userData.password !== pass.value) {
+                // Invalid password
+                setErrorMessages({ name: "pass", message: errors.pass });
+            } else {
+                setIsSubmitted(true);
+            }
+        } else {
+            // Username not found
+            setErrorMessages({ name: "uname", message: errors.uname });
+        }
+    };
+
+    //Generate JSX code for error message
+    const renderErrorMessage = (name) =>
+        name == errorMessages.name && (
+            <div className='error'>{errorMessages.message}</div>
+        );
+
+
+    const renderForm = (
+        <div className='form'>
+            <form onSumbit={handleSumbit}>
+                <div className='input-container'>
+                <label> usename</label>
+                <input type='text' name='uname' required></input>
+                {this.renderErrorMessage("uname")}
+                </div>
+            <div className='input-container'>
+                <label> passpwrd</label>
+                <input type='password' name='pass' required></input>
+                {this.renderErrorMessage('pass')}
+            </div>
+            <div className='button-container'>
+                <input type='submit'></input>
+            </div>
+            </form>
+           </div>
+    )
+
+const handleSumbit = (event) => {
+    //prevents page relead
+    event.preventDefault();
+};
+
+    return(
+        <div className= "Innlogging" >
+            <div className="login-form">
+                <div className="title">Sign In</div>
+                {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+            </div>
+    </div>
+  );
+}
+
+
+
+export default Innlogging;
+
+
+/*
+
+    render() {
+        return (
+            <div>
+            <div> <h1> hello world, her kommer innlogging</h1></div>
+            </div>
+        )
+    };
+};*/
