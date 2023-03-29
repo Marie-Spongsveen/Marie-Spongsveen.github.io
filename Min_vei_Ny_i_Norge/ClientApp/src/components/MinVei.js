e8b5c2c6251eff180d7931095f8445fce390519e
Marie Spongsveen <marie.spongsveen@gmail.com>
2023-03-29 11:40:06 +0200

    har fått til å vise nedtrekkslisten kun når spørsmål 1 er hentet

--- Min_vei_Ny_i_Norge/ClientApp/src/components/MinVei.js
+++ Min_vei_Ny_i_Norge/ClientApp/src/components/MinVei.js
@@ -1,21 +1,24 @@
 import { useState } from "react";
 import axios from 'axios';
-import { LandNedtrekksliste } from './landNedtrekkslite'
+import { LandNedtrekksliste } from './landNedtrekksliste'
 
 export const MinVei = () => {
     let [id, setId] = useState(1);
     const [sporsmal, setSporsmal] = useState();
+    const [landSynlighet, setLandSynlighet] = useState(false)
+
+    const visibility = landSynlighet ? 'visible' : 'hidden';
+
 
     const hentSporsmal = () => {
         axios.get('hent/' + id)
             .then((response: AxiosResponse<any>) => {
                 setSporsmal(response.data)
             });
-
-        //hentSvaralternativer;
     }
 
     const hentSvaralternativer = () => {
+        console.log(id)
         axios.get('hentsvaralternativer/' + id)
             .then((response: AxiosResponse<any>) => {
                 console.log(response.data)
@@ -24,41 +27,61 @@ export const MinVei = () => {
 
     const neste = () => {
         id = parseInt(id);
-        setId(i => Math.min(i + 1, 8));
+        setId(i => Math.min(i + 1, 9));
+        console.log(id)
 
         axios.get('hent/' + id)
             .then((response: AxiosResponse<any>) => {
                 setSporsmal(response.data)
             });
+
+        fikseSvar();
     }
 
     const forrige = () => {
         id = parseInt(id);
-
         setId(i => Math.max(i - 1, 1));
+        console.log(id)
 
         axios.get('hent/' + id)
             .then((response: AxiosResponse<any>) => {
                 setSporsmal(response.data)
             });
+
+        fikseSvar();
     }
+
+    const fikseSvar = () => {
+        switch (id) {
+            case 1:
+                setLandSynlighet(true);
+            case 2: console.log('id2')
+        }
+
+
+        // eller bedre å bruke en switch??
+        if (id == 1) {
+        }
+        else setLandSynlighet(false);
+        //hentSvaralternativer;
+
+        console.log(landSynlighet)
+
+        
+    }
+    
 
     return (
         <div>
-            <div>
-                <label>Hvilket spørsmål nummer vil du ha?</label>
-                <input
-                    type="number"
-                    onChange={(event) => setId(event.target.value)}>
-                </input>
-            </div>
             <button onClick={hentSporsmal}>Hent spørsmål {id}</button>
             <div>Spørsmålet: {sporsmal}</div>
 
             <button onClick={forrige}>Back</button>
             <button onClick={neste}>Next</button>
 
-            <LandNedtrekksliste />
+            <div style={{ visibility }}>
+                <LandNedtrekksliste />
+            </div>
         </div>
     );
 }
