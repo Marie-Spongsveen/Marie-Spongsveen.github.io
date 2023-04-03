23c2b085e72583bc1d780471d1f84f137a289288
Marie Spongsveen <marie.spongsveen@gmail.com>
2023-04-03 11:07:26 +0200

    begynt å lage litt generell css

--- Min_vei_Ny_i_Norge/ClientApp/src/components/Home.js
+++ Min_vei_Ny_i_Norge/ClientApp/src/components/Home.js
@@ -8,27 +8,25 @@ export class Home extends Component {
     render() {
         return (
             <div>
-                <div class='boks'>
-                    <div class='headline'>
-                        <h1>My digital Guide</h1>
+                <div className='boks'>
+                    <div className='headline'>
+                        <h1>My digital guide</h1>
                     </div>
-                    <div class='infotekst'>
+                    <div className='infotekst tekstboks'>
                         <p> This guide is a simulation of your duties, right and benefits. In this guide you will be asked to answer a few questions based on your situation. All the questions are optional, but we recommend answering the ones you can.
                             We do not permanently store any of the information you provide. This not an application, if you wish to apply you need to contact the responsible agency.</p>
                     </div>
-                    <div class='headline'>
-                        <h1>Optional log in</h1></div>
-                    <div class='infotekst'>
+                    <div className='headline'>
+                        <h2>Optional log in</h2></div>
+                    <div className='infotekst tekstboks'>
                         <p>In order to offer you relevant information, you have the option to log in. By logging in some of the questions can be filled out for you. You can use a Norwegian or European ID to log in.
                             You can also choose to continue without logging in.</p>
                     </div>
                 </div>
 
                 <div class='knappContainer'>
-                    <div class='knapp'> <p>Continue without login</p> </div>
-                    <div class='knapp'><svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M489 936v-60h291V276H489v-60h291q24 0 42 18t18 42v600q0 24-18 42t-42 18H489Zm-78-185-43-43 102-102H120v-60h348L366 444l43-43 176 176-174 174Z" /></svg> Login </div>
-
-
+                    <button className='knapp'>Continue without login</button>
+                    <button className='knapp'><svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M489 936v-60h291V276H489v-60h291q24 0 42 18t18 42v600q0 24-18 42t-42 18H489Zm-78-185-43-43 102-102H120v-60h348L366 444l43-43 176 176-174 174Z"/></svg>Login</button>
                 </div>
             </div>
 
--- Min_vei_Ny_i_Norge/ClientApp/src/components/Layout.js
+++ Min_vei_Ny_i_Norge/ClientApp/src/components/Layout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 import { Footer } from './Footer';
+import './Layout.css';
 
 export class Layout extends Component {
     static displayName = Layout.name;
@@ -10,9 +11,11 @@ export class Layout extends Component {
         return (
             <div>
                 <NavMenu />
-                <Container>
-                    {this.props.children}
-                </Container>
+                <div className="main">
+                    <Container>
+                        {this.props.children}
+                    </Container>
+                </div>
                 <Footer />
             </div>
         );
