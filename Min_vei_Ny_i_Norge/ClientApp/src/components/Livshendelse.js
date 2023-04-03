d237229579fe37122f423965ada2d666e77ccf39
Marie Spongsveen <marie.spongsveen@gmail.com>
2023-04-03 09:55:16 +0200

    brukt faktisk props og laget komponenter av hver av livshendels boksene

---
+++ Min_vei_Ny_i_Norge/ClientApp/src/components/Livshendelse.js
@@ -0,0 +1,13 @@
+﻿import './VelgLivssituasjon.css'
+
+export const Livshendelse = (props) => {
+    return (
+        <div className="livssituasjon-boks">
+            <div className="livssituasjon-boks-overskrift">
+                <img src={props.ikon}></img>
+                <h3>{props.overskrift}</h3>
+            </div>
+            <p>{props.tekst}</p>
+        </div>
+    );
+}
--- Min_vei_Ny_i_Norge/ClientApp/src/components/VelgLivssituasjon.js
+++ Min_vei_Ny_i_Norge/ClientApp/src/components/VelgLivssituasjon.js
@@ -1,72 +1,61 @@
-﻿import { react } from 'react'
-import barnIkon from '../bilder/barn_ikon.png'
-import buisnessIkon from '../bilder/business_ikon.png'
-import dødArvIkon from '../bilder/død_arv_ikon.png'
-import frivilligIkon from '../bilder/frivillig_ikon.png'
-import jobbIkon from '../bilder/jobb_ikon.png'
-import nyINorgeIkon from '../bilder/ny_norge_ikon.png'
-import syktBarnIkon from '../bilder/sykt_barn_ikon.png'
+﻿import { Livshendelse } from './Livshendelse'
+import { TilbakeKnapp } from './Knapper/TilbakeKnapp'
+
 import './VelgLivssituasjon.css'
+
+import barnIkon from '../Bilder/barn_ikon.png'
+import buisnessIkon from '../Bilder/business_ikon.png'
+import dødArvIkon from '../Bilder/død_arv_ikon.png'
+import frivilligIkon from '../Bilder/frivillig_ikon.png'
+import jobbIkon from '../Bilder/jobb_ikon.png'
+import nyINorgeIkon from '../Bilder/ny_norge_ikon.png'
+import syktBarnIkon from '../Bilder/sykt_barn_ikon.png'
 
 export const VelgLivssituasjon = () => {
     return (
         <div>
-            <button>Back</button>
+            <TilbakeKnapp />
 
             <div className="velg-livssituasjon-side">
                 <h1>My Digital Guide</h1>
                 <h2>Choose your life situation</h2>
 
                 <div className="livssituasjonene">
-                    <div className="livssituasjon-boks">
-                        <div className="livssituasjon-boks-overskrift">
-                            <img src={nyINorgeIkon}></img>
-                            <h3>New in Norway</h3>
-                        </div>
-                        <p>You are moving to Norway temporarily or permanently due to work, studies, family or other reasons.</p>
-                    </div>
-                    <div className="livssituasjon-boks">
-                        <div className="livssituasjon-boks-overskrift">
-                            <img src={barnIkon} className="barn-ikon"></img>
-                            <h3>Have a child</h3>
-                        </div>
-                        <p>You are expecting or have recently had a child.</p>
-                    </div>
-                    <div className="livssituasjon-boks">
-                        <div className="livssituasjon-boks-overskrift">
-                            <img src={jobbIkon}></img>
-                            <h3>Loose and Find a job</h3>
-                        </div>
-                        <p>You have lost your job or are looking for new work.</p>
-                    </div>
-                    <div className="livssituasjon-boks">
-                        <div className="livssituasjon-boks-overskrift">
-                            <img src={dødArvIkon}></img>
-                            <h3>Death and inheritance</h3>
-                        </div>
-                        <p>You are planning the last phase of someone's life or are a caregiver of someone seriously ill or injured.</p>
-                    </div>
-                    <div className="livssituasjon-boks">
-                        <div className="livssituasjon-boks-overskrift">
-                            <img src={syktBarnIkon}></img>
-                            <h3>Seriously ill child</h3>
-                        </div>
-                        <p>You have a chil with needs, chronically ill child or child in hospital.</p>
-                    </div>
-                    <div className="livssituasjon-boks">
-                        <div className="livssituasjon-boks-overskrift">
-                            <img src={frivilligIkon}></img>
-                            <h3>Start and run a voluntary organization</h3>
-                        </div>
-                        <p>You want to start your own voluntary organization or you already have one.</p>
-                    </div>
-                    <div className="livssituasjon-boks">
-                        <div className="livssituasjon-boks-overskrift">
-                            <img src={buisnessIkon}></img>
-                            <h3>Start and run a business</h3>
-                        </div>
-                        <p>You want to start your own business or you already have your own.</p>
-                    </div>
+                    <Livshendelse
+                        ikon={nyINorgeIkon}
+                        overskrift="New in Norway"
+                        tekst="You are moving to Norway temporarily or permanently due to work, studies, family or other reasons."
+                    />
+                    <Livshendelse
+                        ikon={barnIkon}
+                        overskrift="Have a child"
+                        tekst="You are expecting or have recently had a child."
+                    />
+                    <Livshendelse
+                        ikon={jobbIkon}
+                        overskrift="Loose and Find a job"
+                        tekst="You have lost your job or are looking for new work."
+                    />
+                    <Livshendelse
+                        ikon={dødArvIkon}
+                        overskrift="Death and inheritance"
+                        tekst="You are planning the last phase of someone's life or are a caregiver of someone seriously ill or injured."
+                    />
+                    <Livshendelse
+                        ikon={syktBarnIkon}
+                        overskrift="Seriously ill child"
+                        tekst="You have a chil with needs, chronically ill child or child in hospital."
+                    />
+                    <Livshendelse
+                        ikon={frivilligIkon}
+                        overskrift="Start and run a voluntary organization"
+                        tekst="You want to start your own voluntary organization or you already have one."
+                    />
+                    <Livshendelse
+                        ikon={buisnessIkon}
+                        overskrift="Start and run a business"
+                        tekst="You want to start your own business or you already have your own."
+                    />
                  </div>
             </div>
         </div>
