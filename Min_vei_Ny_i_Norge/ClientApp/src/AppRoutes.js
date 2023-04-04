ce981ec27f4d364f854bbca585ba4ccc085411b4
Kjetil Leivseth <s304147@oslomet.no>
2023-04-04 09:46:14 +0200

    commit før merge

--- Min_vei_Ny_i_Norge/ClientApp/src/AppRoutes.js
+++ Min_vei_Ny_i_Norge/ClientApp/src/AppRoutes.js
@@ -1,15 +1,22 @@
 import { Home } from "./components/Home";
 import { MinVei } from "./components/MinVei";
+import { MeldeFlytte } from "./components/MeldeFlytte";
 
 const AppRoutes = [
-  {
-    index: true,
-    element: <Home />
-  },
-  {
-    path: '/min-vei',
-    element: <MinVei />
-  }
+    {
+        index: true,
+        element: <Home />
+    },
+    
+    {
+        path: '/min-vei',
+        element: <MinVei />
+    },
+    {
+        path: '/meldeFytte',
+        element: <MeldeFlytte />
+    }
 ];
+
 
 export default AppRoutes;
