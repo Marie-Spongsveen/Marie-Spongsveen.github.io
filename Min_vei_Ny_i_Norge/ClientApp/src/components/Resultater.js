f4b9589f14b2618bdaf57d76bc00af3768292386
Kjetil Leivseth <s304147@oslomet.no>
2023-04-12 13:42:14 +0200

    Delte opp i flere komponenter for mer oversiktlig kode - plikter, rettigheter, informasjon og aarene fremover
    Pliktene skal genereres dynamisk etterhvert, resten er statisk

--- Min_vei_Ny_i_Norge/ClientApp/src/components/Resultater.js
+++ Min_vei_Ny_i_Norge/ClientApp/src/components/Resultater.js
@@ -1,12 +1,13 @@
-﻿import React from 'react';
+﻿//bør vurdere om komponentene skal splittes ut i flere filer i en ny mappe??
 
+import React from 'react';
 
-export const Resultater = () => {
+//bygger resultatsiden ved hjelp av komponentene under denne
+export default const Resultater = () => {
     return (
         <div>
             <TilbakeKnapp />
 
-            //dynamisk del med komponenter
             <div className="mine-plikter-del">
                 <h1>My Results</h1>
                 <h2>My Duties</h2>
@@ -21,92 +22,120 @@ export const Resultater = () => {
 
             </div>
 
-            //statisk del
-            <div className="mine-rettigheter-del">
-                <h2>My Benefits</h2>
-
-                <div className="mine-rettigheter">
-                    <Rettigheter
-                        ikon={navLogo}
-                        overskrift="National insurance scheme"
-                        tekst="You will be a member of the scheme because you are working in Norway. Membership in the National Insurance Scheme is the key to eligibility for rights to services from NAV. The scheme covers among other things health benefits, pension and parental benefits."
-                        lenke="Membership of the National Insurance Scheme - nav.no"
-                        lenke="Information about NAV's services and benefits - nav.no"
-                    />
-                    <Rettigheter
-                        ikon={arbeidstilsynetLogo}
-                        overskrift="Labor rights"
-                        tekst="When you work in Norway you will have multiple rights for example about working hours, tax or safe working environments."
-                        lenke="Working in Norway: Your rights and obligations (arbeidstilsynet.no)"
-                    />
-                    <Rettigheter
-                        ikon={navLogo}
-                        overskrift="Pension rights"
-                        tekst="Norwegian pension is a part of the national insurance scheme. The government will start saving up for you when you are paying tax to Norway."
-                        lenke="Pensions and pension applications from outside Norway - nav.no"
-                    />
-                    
-                </div>
+            <div>
+                <MineRettigheter />
             </div>
 
-            //statisk del
-            <div className="informasjon-relevant-del">
-                <h2>Information we think may be relevant for you</h2>
-
-                <div className="informasjon-relevant">
-                    <Informasjon
-                        ikon={udiLogo}
-                        overskrift="Bringing your family with you"
-                        tekst="As an EU or EEA citizen you have the right to bring your family to Norway, but they may have to apply to move themselves."
-                    />
-                    <Informasjon
-                        ikon={norgeskart}
-                        overskrift="Norwegian language course"
-                        tekst="Learning Norwegian makes it easier to navigate through the city, understand the culture and getting involved in your local community. Some municipals arrange language courses, you will have to check your municipal."
-                    />
-                    <Informasjon
-                        ikon={statensVegvesenLogo}
-                        overskrift="Exchanging your driver's license"
-                        tekst="If you have a driving license issued in another EU/EEA country, it is valid for driving in Norway providing the licence is still valid. If you are living permanently in Norway, you can use your driving license for driving in Norway or exchange it for a Norwegian driving license."
-                    />
-                    <Informasjon
-                        ikon={statensVegvesenLogo}
-                        ikon2={tollLogo}
-                        overskrift="Bringing your vehicle"
-                        tekst="If your vehicle meets Norwegian and European technical requirements, you can bring your car. Register the vehicle with customs when crossing the border. Tolls and taxes apply."
-                    />
-                </div>
+            <div>
+                <NyttigInformasjon />
             </div>
 
-            //statisk del
-            <div className="videre-fremover-del">
-                <h2>The years ahead</h2>
-
-                <div className="videre-fremover">
-                    <Fremover
-                        ikon={riksVåpen}
-                        overskrift="Voting rights in local elections"
-                        tekst="If you have lived in Norway for at least three consecutive years before the date of the election, you gain the right to vote in the local elections. Local elections are considered as municipal and county council elections."
-                    />
-                    <Fremover
-                        ikon={udiLogo}
-                        overskrift="Permanent residency"
-                        tekst="If you have stayed in Norway as an EU/EEA national for at least five years, you can apply for permanent right of residence. This entitles you to stay and work in Norway indefinitely. Your family members may also be eligble."
-                    />
-                    <Fremover
-                        ikon={udiLogo}
-                        overskrift="Norwegian citizenship"
-                        tekst="The requirements for Norwegian citizenship are not the same for everyone. You have to have permanent recidency in Norway. A Norwegian citizenship can give you more rights."
-                    />
-                    
-                </div>
+            <div>
+                <VidereFremover />
             </div>
 
         </div>
     );
 }
 
+//statisk del for "My benefits"
+const MineRettigheter = () => {
+    return (
+        
+        <div className="mine-rettigheter-del">
+            <h2>My Benefits</h2>
 
+            <div className="mine-rettigheter">
+                <Rettigheter
+                    ikon={navLogo}
+                    overskrift="National insurance scheme"
+                    tekst="You will be a member of the scheme because you are working in Norway. Membership in the National Insurance Scheme is the key to eligibility for rights to services from NAV. The scheme covers among other things health benefits, pension and parental benefits."
+                    lenke="Membership of the National Insurance Scheme - nav.no"
+                    lenke="Information about NAV's services and benefits - nav.no"
+                />
+                <Rettigheter
+                    ikon={arbeidstilsynetLogo}
+                    overskrift="Labor rights"
+                    tekst="When you work in Norway you will have multiple rights for example about working hours, tax or safe working environments."
+                    lenke="Working in Norway: Your rights and obligations (arbeidstilsynet.no)"
+                />
+                <Rettigheter
+                    ikon={navLogo}
+                    overskrift="Pension rights"
+                    tekst="Norwegian pension is a part of the national insurance scheme. The government will start saving up for you when you are paying tax to Norway."
+                    lenke="Pensions and pension applications from outside Norway - nav.no"
+                />
+
+            </div>
+        </div>
+    )
+
+}
+
+//statisk del for "Information we think may be relevant for you"
+const NyttigInformasjon = () => {
+    return (
+
+        <div className="informasjon-relevant-del">
+            <h2>Information we think may be relevant for you</h2>
+
+            <div className="informasjon-relevant">
+                <Informasjon
+                    ikon={udiLogo}
+                    overskrift="Bringing your family with you"
+                    tekst="As an EU or EEA citizen you have the right to bring your family to Norway, but they may have to apply to move themselves."
+                />
+                <Informasjon
+                    ikon={norgeskart}
+                    overskrift="Norwegian language course"
+                    tekst="Learning Norwegian makes it easier to navigate through the city, understand the culture and getting involved in your local community. Some municipals arrange language courses, you will have to check your municipal."
+                />
+                <Informasjon
+                    ikon={statensVegvesenLogo}
+                    overskrift="Exchanging your driver's license"
+                    tekst="If you have a driving license issued in another EU/EEA country, it is valid for driving in Norway providing the licence is still valid. If you are living permanently in Norway, you can use your driving license for driving in Norway or exchange it for a Norwegian driving license."
+                />
+                <Informasjon
+                    ikon={statensVegvesenLogo}
+                    ikon2={tollLogo}
+                    overskrift="Bringing your vehicle"
+                    tekst="If your vehicle meets Norwegian and European technical requirements, you can bring your car. Register the vehicle with customs when crossing the border. Tolls and taxes apply."
+                />
+            </div>
+        </div>
+    )
+}
+
+//statisk del
+const VidereFremover = () => {
+    return (
+
+        <div className="videre-fremover-del">
+            <h2>The years ahead</h2>
+
+            <div className="videre-fremover">
+                <Fremover
+                    ikon={riksVåpen}
+                    overskrift="Voting rights in local elections"
+                    tekst="If you have lived in Norway for at least three consecutive years before the date of the election, you gain the right to vote in the local elections. Local elections are considered as municipal and county council elections."
+                />
+                <Fremover
+                    ikon={udiLogo}
+                    overskrift="Permanent residency"
+                    tekst="If you have stayed in Norway as an EU/EEA national for at least five years, you can apply for permanent right of residence. This entitles you to stay and work in Norway indefinitely. Your family members may also be eligble."
+                />
+                <Fremover
+                    ikon={udiLogo}
+                    overskrift="Norwegian citizenship"
+                    tekst="The requirements for Norwegian citizenship are not the same for everyone. You have to have permanent recidency in Norway. A Norwegian citizenship can give you more rights."
+                />
+
+            </div>
+        </div>
+    )
+}
+
+//dynamisk komponent
 function MeldeFlytte = () => {
     return (
         <div>
@@ -131,6 +160,7 @@ function MeldeFlytte = () => {
         </div>
 }
 
+//dynamisk komponent
 function RegHosPolitiet = () => {
     return (
         <div>
@@ -153,6 +183,7 @@ function RegHosPolitiet = () => {
         </div>
 }
 
+//dynamisk komponent
 function IdentitetsNummer = () => {
     return (
         <div>
@@ -171,6 +202,7 @@ function IdentitetsNummer = () => {
         </div>
 }
 
+//dynamisk komponent
 function Skattekort = () => {
     return (
         <div>
@@ -195,4 +227,3 @@ function Skattekort = () => {
         </div>
 }
 
-//dele opp i komponenter for mer oversiktlig kode - plikter, rettigheter, informasjon og årene fremover 
