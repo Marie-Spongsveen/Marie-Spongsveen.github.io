215497646a3c6b10dcbb4ff0b98f17d0476e307d
Marie Spongsveen <marie.spongsveen@gmail.com>
2023-04-19 13:34:26 +0200

    lagt inn litt kommentarer begynt å style siden noe og legge inn overskriter begynt å legge inn muligheten for to statsborgerskap

--- Min_vei_Ny_i_Norge/ClientApp/src/components/MinVei.js
+++ Min_vei_Ny_i_Norge/ClientApp/src/components/MinVei.js
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 import { LandNedtrekksliste } from './landNedtrekksliste'
 import { Knapp } from './Knapp'
 
+import "./MinVei.css"
+
 export const MinVei = () => {
+    const naviger = useNavigate();
+
     const [id, setId] = useState(1);
     const [sporsmal, setSporsmal] = useState();
     const [svaralternativ, setSvaralternativ] = useState();
@@ -12,25 +17,36 @@ export const MinVei = () => {
     const [forrigeSynlighet, setForrigeSynlighet] = useState(false);
     const [nesteSynlighet, setNesteSynlighet] = useState(true);
 
-    const [landlisteSynlighet, setLandlisteSynlighet] = useState(false);
+    const [landlisteSynlighet, setLandlisteSynlighet] = useState(true);
     const [resultKnappSynlighet, setResultatKnappSynlighet] = useState(false);
+    const [tilbakeLivshendelseKnappSynlighet, setTilbakeLivshendelseKnappSynlighet] = useState(true)
+
+    const [andreLandListe, setAndreLandListe] = useState(false)
 
     // hook som inneholder JSONobjekter med hva brukeren svarer på spørsmålene
-    const [svartData, setSvartData] = useState({});
+    const [svarData, setSvarData] = useState({});
 
     useEffect(() => {
         hentSporsmal()
-        console.log(svartData)
+
+        console.log(svarData)
+        for (let i in svarData) {
+            console.log(i + "    " + svarData[i])
+        }
 
         // sjekker hvilket spørsmål man er på, 
         // og gjør de riktige elemtene synlige/usynlige
         switch (id) {
             case 1: 
                 setLandlisteSynlighet(true)
-                setForrigeSynlighet(false)
+                setTilbakeLivshendelseKnappSynlighet(true)
+                setForrigeSynlighet(true)
                 break
             case 2:
                 setForrigeSynlighet(true)
+                setTilbakeLivshendelseKnappSynlighet(false)
+                break
+            case 3:
                 setLandlisteSynlighet(false)
                 break
             case 6:
@@ -62,7 +78,7 @@ export const MinVei = () => {
     }
 
     const handleChange = (event) => {
-        setSvartData(prevFormData => {
+        setSvarData(prevFormData => {
             return {
                 ...prevFormData,
                 [event.target.name]: event.target.value
@@ -81,51 +97,89 @@ export const MinVei = () => {
         });
 
     const neste = () => {
+        /* Setter id til 1 større for å gå til neste spørsmål */
         if (id < 7) setId(prevId => prevId + 1) // 7 må byttes ut med en maks id, eller si når gå til resultat knappen ikke er der
     }
 
     const forrige = () => {
+        /* Setter id til en midnre for å gå til forrige spørsmål eller forrige side
+             Hvis id er større enn 1 blir 1 satt til en mindre, og man går til forrige spørsmål 
+             Hvis id == 1 så er man på første spørsmål og går da tilbake til forrige side */
         if (id > 1) setId(prevId => prevId - 1)
+        else if (id == 1) naviger("/velg-livssituasjon")
     }
+
 
     // denne er ikke testet
     const gaTilResultat = () => {
-        axios.post('svar/', svartData)
+        /* Sender brukerens svar til bakcken og navigerer brukeren til resultatsiden */
+        axios.post('svar/', svarData)
             .then((response: AxiosResponse<any>) => {
                 console.log(response)
             });
+        naviger("/resultat")
     }
 
+    const hei = () => { setAndreLandListe(prev => !prev) }
+
     return (
-        <div>
-            <div>Question: { sporsmal }</div>
+        <div className="minVei">
+            <h1>My Digital Guide</h1>
+            <h2>New in Norway</h2>
+
+            { /* Må legge inn det brukeren har svart her */ }
+
+            { /* Viser ett og ett spørsmål med svaralternativer eller eventuelle input */ }
+            <h3>{ sporsmal }</h3>
             { svarene }
 
-            { landlisteSynlighet && <LandNedtrekksliste /> } 
+            { /* Viser nedtrekkslisten med land kun om hooken landlisteSynlighet er true */
+                landlisteSynlighet &&
+                    <div>
+                        <LandNedtrekksliste />
+                        <label>
+                            <input type="checkbox" value={andreLandListe}></input>
+                            I have multiple citizenships
+                        </label>
+                    </div>
+            }
 
-            <div>
-                { forrigeSynlighet && 
-                    <Knapp
-                        navn="Tilbake"
-                        handleClick={ forrige }
-                        handleClassName="tilbakeKnapp"
-                    />
+            {
+                andreLandListe &&
+                    <div>
+                        <h3>What is your second citizenship?</h3>
+                        <LandNedtrekksliste />
+                    </div>
+            }
+
+            <div className="knapper">
+                { /* Knapp for å gå tilbake
+                         Hvis man er på første spørsmål går man tilbake til forrige siden
+                         Hvis det er et forrige spørsmål, går man tilbake til det */
+                    forrigeSynlighet && 
+                        <Knapp
+                            navn="Back"
+                            handleClick={ forrige }
+                            handleClassName="tilbakeKnapp"
+                        />
                 }
 
-                { nesteSynlighet &&
-                    <Knapp
-                        navn="Neste"
-                        handleClick={ neste }
-                        handleClassName="nesteKnapp"
-                    />
+                {
+                    nesteSynlighet &&
+                        <Knapp
+                            navn="Next"
+                            handleClick={ neste }
+                            handleClassName="nesteKnapp"
+                        />
                 }
 
-                { resultKnappSynlighet &&
-                    <Knapp
-                        navn="Go to my results"
-                        handleClick={ gaTilResultat }
-                        handleClassName="nesteKnapp"
-                    />
+                {
+                    resultKnappSynlighet &&
+                        <Knapp
+                            navn="Go to my results"
+                            handleClick={ gaTilResultat }
+                            handleClassName="nesteKnapp"
+                        />
                 }
             </div>
         </div>
