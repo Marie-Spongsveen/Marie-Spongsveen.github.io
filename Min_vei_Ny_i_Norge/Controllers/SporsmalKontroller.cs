8066bb10b8bf205478497affddd9b2e62d01b6ca
idachr <chr.ida@hotmail.com>
2023-04-24 09:39:41 +0200

    Update SporsmalKontroller.cs

--- Min_vei_Ny_i_Norge/Controllers/SporsmalKontroller.cs
+++ Min_vei_Ny_i_Norge/Controllers/SporsmalKontroller.cs
@@ -16,6 +16,9 @@ namespace Min_vei_Ny_i_Norge.Controllers
             _db = db;
         }
 
+        //hey
+        //hey
+
         [HttpGet]
         [Route("/hent/{id}")]
         public async Task<string> hent(int id)
